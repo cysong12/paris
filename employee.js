52fd1780934dab11a79a420e7b7fb472085e5339
choi <cysong12@gmail.com>
2019-12-25 17:20:00 -0800

    aa

--- employee.js
+++ employee.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
         db.collection("employees").get().then(function(querySnapshot) {
             querySnapshot.forEach(function(doc) {
                 // doc.data() is never undefined for query doc snapshots
-                if (doc.data().name !== '') {
+                if (doc.data().name !== '' && doc.data().name !== 'undefined') {
                     j = i + 1;
                     form = document.getElementById("usr" + j.toString());
                     form.setAttribute('value', doc.data().name);
@@ -31,14 +31,33 @@ $(document).ready(function(){
             let names = document.getElementsByTagName("INPUT");
             //console.log(names[0].value);
             for (let i = 0; i < names.length; i++) {
-                if (names[i].value.replace(/\s+/g, '') != '') {
-                    db.collection("employees").doc(i.toString()).set({
-                        name: names[i].value.replace(/\s+/g, ''),
-                        wage: 0
+                //if (names[i].value.replace(/\s+/g, '') != '') {
+                    db.collection("employees").doc(i.toString()).get().then(function(doc) {
+                        if (!doc.exists && names[i].value.replace(/\s+/g, '') !== '') {
+                            console.log('here');
+                            db.collection("employees").doc(i.toString()).set({
+                                name: names[i].value.replace(/\s+/g, ''),
+                                wage: 0
+                            });
+                        }
+                        else if (doc.exists && names[i].value.replace(/\s+/g, '') === '') {
+                            console.log('yellow');
+                            db.collection("employees").doc(i.toString()).delete();
+                        }
+                        else if (doc.exists && doc.data().name !== names[i].value.replace(/\s+/g, '')) {
+                            console.log('there');
+                            db.collection("employees").doc(i.toString()).set({
+                                name: names[i].value.replace(/\s+/g, ''),
+                                wage: 0
+                            });
+                        }
+                        else {
+                            console.log('yonder');
+                        }
                     });
-                }
+                //}
             }
-            window.alert("저장이 완료되었습니다");
+            window.alert("저장이 완료되었습니다");      //timeout of 5 seconds
                 
             
 
--- input.js
+++ input.js
@@ -3,28 +3,104 @@ $(document).ready(function(){
     let db = firebase.firestore();
 
     let name, label, wage;
+    let dayName, month, day, year;
+    let dayNameKR, monthKR, dayKR, yearKR;
+    let employee_count;
 
-    //readEmployeeDb();
+    let queryStringURL = decodeURIComponent(window.location.search);
+    let tempVar = queryStringURL.split('?');
+    let date = tempVar[1];
+    console.log(date);
+
+    dayName = date.substring(0, 3);
+    month = date.substring(3, 6);
+    day = date.substring(6, 8);
+    year = date.substring(8, 12);
+
+    dayKR = day + '일';
+    yearKR = year + '년';
+
+    switch(dayName) {
+        case "Mon":
+            dayNameKR = '(월)';
+            break;
+        case "Tue":
+            dayNameKR = '(화)';
+            break;
+        case "Wed":
+            dayNameKR = '(수)';
+            break;
+        case "Thu":
+            dayNameKR = '(목)';
+            break;
+        case "Fri":
+            dayNameKR = '(금)';
+            break;
+        case "Sat":
+            dayNameKR = '(토)';
+            break;
+        case "Sun":
+            dayNameKR = '(일)';
+            break;
+    }
+
+    switch(month) {
+        case "Jan":
+            monthKR = '1월';
+            break;
+        case "Feb":
+            monthKR = '2월';
+            break;
+        case "Mar":
+            monthKR = '3월';
+            break;
+        case "Apr":
+            monthKR = '4월';
+            break;
+        case "May":
+            monthKR = '5월';
+            break;
+        case "Jun":
+            monthKR = '6월';
+            break;
+        case "Jul":
+            monthKR = '7월';
+            break;
+        case "Aug":
+            monthKR = '8월';
+            break;
+        case "Sep":
+            monthKR = '9월';
+            break;
+        case "Oct":
+            monthKR = '10월';
+            break;
+        case "Nov":
+            monthKR = '11월';
+            break;
+        case "Dec":
+            monthKR = '12월';
+            break;
+    }
+
+    console.log(dayName, month, day, year);
+    document.getElementById("dateP").innerHTML = yearKR + ' ' + monthKR + ' ' + dayKR + ' ' + dayNameKR;
+
+    readEmployeeDb();
 
     let names = document.getElementsByClassName("user");
-
-    $('#input_starttime').pickatime({
-        default: 'now',
-        editable: true,
-    });
+    let wageArr = [];
 
     function readEmployeeDb() {
-        let i = 0; 
-        let j;
+        let i = 0;
+        employee_count = 0; 
         db.collection("employees").get().then(function(querySnapshot) {
             querySnapshot.forEach(function(doc) {
                 // doc.data() is never undefined for query doc snapshots
                 if (doc.data().name !== '') {
-                    j = i + 1;
-                    label = document.getElementById("user" + j.toString());
-                    label.innerText = doc.data().name;
-                    wage = document.getElementById("usr" + j.toString());
-                    wage.setAttribute("VALUE", doc.data().wage);
+                    names[i].innerHTML = doc.data().name + ' (₩' + doc.data().wage + ')';
+                    wageArr.push(doc.data().wage);
+                    employee_count++;
                 }
                 i++;
             });
@@ -33,4 +109,54 @@ $(document).ready(function(){
             console.log("Error getting documents: ", error);
         });
     }
+
+    let startTimes = document.getElementsByClassName("startTime");
+    let endTimes = document.getElementsByClassName("endTime");
+    let duration, startM, startH, endM, endH;
+    let startTemp, endTemp;
+    console.log(startTimes[0].value);
+
+    $("#saveButton").click(function() {
+        console.log(employee_count);
+        for (i = 0; i < employee_count; i++) {
+            startTemp = startTimes[i].value;
+            endTemp = endTimes[i].value;
+            differenceArr = differenceCalculate(startTemp, endTemp);
+            difference = differenceArr[0];
+            differencePrint = differenceArr[1];
+            console.log(startTemp, endTemp);
+            console.log(difference);
+            console.log(wageArr[i]);
+            db.collection("employees").doc(i.toString()).collection("timetable").doc(date).set({
+                startTime: startTemp,
+                endTime: endTemp,
+                duration: differencePrint,
+                dayIncome: difference / 60 * wageArr[i]
+            });
+        }
+    });
+
+    let endTotalMinutes, startTotalMinutes, durationMinute, durationHour;
+
+    function differenceCalculate(start, end) {
+        startH = Number(start.substring(0,2));
+        startM = Number(start.substring(3, 5));
+        endH = Number(end.substring(0,2));
+        endM = Number(end.substring(3, 5));
+        console.log(startH, startM, endH, endM);
+        endTotalMinutes = endH * 60 + endM;
+        startTotalMinutes = startH * 60 + startM;
+        durationInMinutes = endTotalMinutes - startTotalMinutes;
+        durationMinute = Math.floor((endTotalMinutes - startTotalMinutes) % 60);
+        durationHour = parseInt((endTotalMinutes - startTotalMinutes - durationMinute) / 60);
+        return [durationInMinutes, durationHour.toString() + ':', durationMinute.toString()];
+    }
+
+    function checkValid() {
+        for (let i = 0; i < employee_count; i++) {
+            
+        }
+    }
+
+    
 });
